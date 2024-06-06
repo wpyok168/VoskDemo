0e96b256926d6be8e8ff6cfa0380dcf075a09f33
wpyok500 <wpyok168@gmail.com>
2024-06-06 22:09:19 +0800

    添加项目文件。

---
+++ VoskDemo/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace VoskDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1199, 967);
+            this.textBox1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1007, 105);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(150, 28);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1007, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 91);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 970);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ VoskDemo/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using Vosk;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace VoskDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VoskTest();
+        }
+        //WasapiCapture wasapi = new WasapiCapture();//麦克风
+        WasapiLoopbackCapture wasapi = new WasapiLoopbackCapture();  //扬声器
+        private void VoskTest()
+        {
+            Task.Run(() => {
+                
+                wasapi.WaveFormat = new NAudio.Wave.WaveFormat(48000, 16, 1);
+                //Vosk.Vosk.SetLogLevel(0);
+                Vosk.Model model = new Vosk.Model("models");
+                VoskRecognizer rec = new VoskRecognizer(model, wasapi.WaveFormat.SampleRate);
+                rec.SetMaxAlternatives(0);
+                rec.SetWords(false);
+                wasapi.DataAvailable += (_, a) =>
+                {
+                    if (rec.AcceptWaveform(a.Buffer, a.BytesRecorded))
+                    {
+                        if (this.textBox1.InvokeRequired)
+                        {
+                            this.textBox1.Invoke(new Action(() =>
+                            {
+                                this.textBox1.AppendText(rec.FinalResult());
+                                
+                            }));
+
+                        }
+                        
+                    }
+                    else if(this.checkBox1.Checked)
+                    {
+                        if (this.textBox1.InvokeRequired)
+                        {
+                            this.textBox1.Invoke(new Action(() =>
+                            {
+                                this.textBox1.AppendText(rec.PartialResult());
+                            }));
+
+                        }
+                    }
+                };
+               
+            });
+            
+        }
+        private delegate void ShowTextBoxtext(string txt);
+        public void showtxt(string txt) 
+        {
+            this.textBox1.AppendText(txt);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wasapi.StartRecording();
+        }
+    }
+}
